4e9ff2f8c6e4e1e7ef5c1204eb8ae747e52b68a3
Abdul Ahad <ahadabdul776aa@gmail.com>
2024-06-19 12:08:23 +0530

    Hi

---
+++ JS/client.js
@@ -0,0 +1,25 @@
+const socket = io('http://localhost:8000');
+
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageInp');
+const messageContainer = document.querySelector(".container");
+
+const append = (message,position) => {
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+}
+
+
+const name1 = prompt("Enter Your Name to Join");
+socket.emit('new-user-joined', name1 )
+
+socket.on('user-joined',name => {
+    append(`${name} joined the chat`,'right')
+})
+
+socket.on('receive',data => {
+    append(`${data.message} : ${data.user} `,'right')
+})
